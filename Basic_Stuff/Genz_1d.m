2188169c4453cc97d5797880ea9c4af052f2adec
Hemaditya Malla <hemadity@jalapeno.sc.cwi.nl>
2018-11-15 10:30:20 +0100

    Working script for different types of 1D Genz functions

--- Basic_Stuff/Genz_1d.m
+++ Basic_Stuff/Genz_1d.m
@@ -1,26 +1,40 @@
-clear;clc;
+%clear;clc;
 
 %Defining the 1D Genz functions
 
-g_continuous = @(xi) exp(-0.5*abs(xi - 0.5));
+%g = @(xi) exp(-0.5*abs(xi - 0.5)); %continuous 
+g = @(xi) 1.0./(0.5^(-2) + (xi - 0.5).^2); %Product Peak 
+%g = @(xi) (1 + 0.5*xi).^(-2); %Corner peak 
+%g = @(xi) (xi > 0.5)*0 + (xi < 0.5).*exp(0.5*xi); %Discontinuous
+%g = @(xi) exp(-(0.5^2)*(xi - 0.5).^2); %Gaussian peak
+%g = @(xi) cos(2*pi*0.5 + 0.5*xi); %Oscillatory
+
+% %Some special G-function
+% a = (1 - 2)/2.0;
+% g = @(xi) (abs(4*xi - 2) + a)/(1 + a); 
+
+
+
 
 %Gauss-Hermite quadrature points (Q points)
-Q = 100;
+Q = 14;
 [xi,w] = gaussQuad(Q,'Hermite');
-g_pts = g_continuous(xi);
+g_pts = g(xi);
 
-ip = (xi(1):0.01:xi(end))'; %Interpolating points
+ip = xi;%(xi(1):0.01:xi(end))'; %Interpolating points
 
 %Number of terms in the polynomial approximation
-N = 15;
+N = 2;
 g_hat = zeros(N,1);
 g_approx = 0;
-for i=1:N
-    g_hat(i,1) = sum(w.*hermite(xi,i).*g_pts)/(factorial(i));
-    g_approx = g_approx + g_hat(i,1)*hermite(ip,i);
+for i=1:N+1
+    g_hat(i,1) = sum(w.*hermite(xi,i-1).*g_pts)/(factorial(i-1));
+    g_approx = g_approx + g_hat(i,1)*hermite(ip,i-1);
 end
 
 plot(xi,g_pts,'.-');
 hold on;
 plot(ip, g_approx,'r');
+xlim([-5,5]);
+ylim([min(g_pts)-0.5,max(g_pts)+0.5]);
 
