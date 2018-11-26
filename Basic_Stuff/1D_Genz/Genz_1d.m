e6676481ea095df4f40936e8dc99b6b10f0322b2
Hemaditya Malla <hemadityamalla@gmail.com>
2018-11-26 08:20:47 +0100

    Minor modifications- changing the number of functions, and error data file

--- Basic_Stuff/1D_Genz/Genz_1d.m
+++ Basic_Stuff/1D_Genz/Genz_1d.m
@@ -2,12 +2,12 @@ clear;clc;
 
 %Defining the 1D Genz functions
 
-%g = @(xi) exp(-0.5*abs(xi - 0.5)); %continuous 
-%g = @(xi) 1.0./(0.5^(-2) + (xi - 0.5).^2); %Product Peak 
-%g = @(xi) (1 + 0.5*xi).^(-2); %Corner peak 
-g = @(xi) (xi > 0.5)*0 + (xi >= 0.5).*exp(0.5*xi); %Discontinuous
-%g = @(xi) exp(-(0.5^2)*(xi - 0.5).^2); %Gaussian peak
-%g = @(xi) cos(2*pi*0.5 + 0.5*xi); %Oscillatory
+%g = @(xi) exp(-0.5*abs(xi)); %continuous 
+%g = @(xi) 1.0./(0.5^(-2) + (xi).^2); %Product Peak 
+%g = @(xi) (1 + 5*xi).^(-2); %Corner peak 
+g = @(xi) (xi > 0)*0 + (xi <= 0).*exp(0.5*xi); %Discontinuous
+%g = @(xi) exp(-(0.5^2)*(xi).^2); %Gaussian peak
+g = @(xi) cos(2*pi*0.5 + 5*xi); %Oscillatory
 
 % %Some special G-function
 % a = (1 - 2)/2.0;
@@ -16,10 +16,10 @@ g = @(xi) (xi > 0.5)*0 + (xi >= 0.5).*exp(0.5*xi); %Discontinuous
 
 
 
-%Gauss-Hermite quadrature points (Q-points)
+%Gauss-Hermite/Legendre quadrature points (Q-points)
 Q = 20;
 [xi,w] = gaussQuad(Q,'Legendre');
-g_pts = g(xi); %Integrates polynomials exactly, but not the Genz functions!
+g_pts = g(xi);
 
 ip = (xi(1):0.01:xi(end))'; %Interpolating points
 
@@ -28,13 +28,14 @@ N = 19;
 g_hat = zeros(N,1);
 g_approx = 0;
 for i=1:N+1
-    g_hat(i,1) = sum(w.*legendre(xi,i-1).*g_pts)/(sum(w.*legendre(xi,i-1).*legendre(xi,i-1)));
+    polynomial = legendre(xi, i-1);
+    g_hat(i,1) = sum(w.*polynomial.*g_pts)/(sum(w.*polynomial.*polynomial));
     g_approx = g_approx + g_hat(i,1)*legendre(ip,i-1);
 end
 
-plot(xi,g_pts,'b.-');
+plot(xi,g_pts,'bo-');
 hold on;
-plot(ip, g_approx,'r');
-xlim([-1,1]);
+plot(ip, g_approx);
+xlim([min(xi)-0.05,max(xi)+0.05]);
 ylim([min(g_pts)-0.5,max(g_pts)+0.5]);
 
