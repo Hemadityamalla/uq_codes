39ebc776a1163b42e3812a8c2ba3d67e6eff6520
Hemaditya Malla <hemadity@jalapeno.sc.cwi.nl>
2018-12-10 15:21:25 +0100

    Modified the program to be able to give multivariate functions as well

--- Basic_Stuff/hermite.m
+++ Basic_Stuff/hermite.m
@@ -1,22 +1,32 @@
 function herm = hermite(pts, degree)
 %Generate the probabilist hermite polynomials at the given 'pt' points and
 %of a given 'degree'. Here the three term recursion is used
-
-%Make sure that pts is a column vector!!!!!!!
-
-if nargin == 1
-    degree = 1; %Gives a default first degree hermite polynomial
+%degree is usually a column of size (number of dimensions)
+%Make sure that pts is a matrix of size (Number of points*number of dimensions)
+[Npts, d] = size(pts);
+%if nargin == 1
+%    degree = 1; %Gives a default first degree hermite polynomial
+%end
+if d == 1
+    if degree == -1
+        herm = zeros(length(pts),1);
+    elseif degree == 0
+        herm = ones(length(pts),1);
+    else
+        herm = pts.*hermite(pts,degree-1) - (degree-1)*hermite(pts,degree-2);
+    end
+    return;
 end
 
 
-
-if degree == -1
-    herm = zeros(length(pts),1);
-elseif degree == 0
-    herm = ones(length(pts),1);
-else
-    herm = pts.*hermite(pts,degree-1) - (degree-1)*hermite(pts,degree-2);
+%The code below can be vectorized-- future work
+polymat = zeros(Npts, d);
+for i_pts = 1:Npts
+    for i_d = 1:d
+       polymat(i_pts,i_d) = hermite(pts(i_pts,i_d),degree(i_d)); 
+    end
 end
 
+herm = prod(polymat,2);
 
 end
