2c23472cb10d8695b2cf19b60b2564ac35afed16
Hemaditya Malla <hemadity@jalapeno.sc.cwi.nl>
2018-12-13 15:32:07 +0100

    Numerical experiments

--- Basic_Stuff/1D_Genz/Genz_1d.m
+++ Basic_Stuff/1D_Genz/Genz_1d.m
@@ -5,11 +5,11 @@ clear;clc;
 g = @(xi) exp(-0.5*abs(xi)); %continuous 
 %g = @(xi) 1.0./(0.5^(-2) + (xi).^2); %Product Peak 
 %g = @(xi) (1 + 5*xi).^(-2); %Corner peak 
-g = @(xi) (xi > 0)*0 + (xi <= 0).*exp(0.5*xi); %Discontinuous
+%g = @(xi) (xi > 0)*0 + (xi <= 0).*exp(0.5*xi); %Discontinuous
 %g = @(xi) exp(-(0.5^2)*(xi).^2); %Gaussian peak
-g = @(xi) cos(2*pi*0.5 + 0.5*xi); %Oscillatory
+%g = @(xi) cos(2*pi*0.5 + 0.5*xi); %Oscillatory
 
-% %Some special G-function
+% %Some special G-functionlegendre
 %a = (1 - 2)/2.0;
 %g = @(xi) (abs(4*xi - 2) + a)/(1 + a); 
 
@@ -18,22 +18,22 @@ g = @(xi) cos(2*pi*0.5 + 0.5*xi); %Oscillatory
 
 error = [];
 %Gauss-Hermite/Legendre quadrature points (Q-points)
-Q = 500;
-[xi,w] = gaussQuad(Q,'Legendre');
+Q = 1000;
+[xi,w] = gaussQuad(Q,'Hermite');
 g_pts = g(xi);
 
 ip = xi;%(xi(1):0.01:xi(end))'; %Interpolating points
 
 %Number of terms in the polynomial approximation
+order = [1,4,9,14,19,24];
 
-
-for N = [1,4,9,19,24]
+for N = order
     g_hat = zeros(round(N),1); 
     g_approx = 0;
     for i=1:N+1
-        polynomial = legendre(xi, i-1);
+        polynomial = hermite(xi, i-1);
         g_hat(i,1) = sum(w.*polynomial.*g_pts)/(sum(w.*polynomial.*polynomial));
-        g_approx = g_approx + g_hat(i,1)*legendre(ip,i-1);
+        g_approx = g_approx + g_hat(i,1)*hermite(ip,i-1);
     end
     %error = [error; abs(g_hat(1,1) - gmean)];
    %error = [error; sqrt(sum((exp(-0.5*ip.^2)/sqrt(2*pi)).*abs(g(ip) - g_approx).^2))];
@@ -50,5 +50,8 @@ plot(ip, g_approx,'r','LineWidth',2);
 xlim([min(xi)-0.05,max(xi)+0.05]);
 ylim([min(g_pts)-0.5,max(g_pts)+0.5]);
 figure(2)
-semilogy([1,4,9,19,24], error, '-o','LineWidth',2,'MarkerSize',8);
+loglog(order, error, '-o','LineWidth',2,'MarkerSize',8);
+hold on;
+p = polyfit(log(order),log(error)',1);
+loglog(order,order.^(-0.5*p(1)),'r','LineWidth',2);
 xlabel('N');ylabel('||g(\xi) - g_N(\xi)||_{L_2}')
--- Basic_Stuff/Xiu_efficient_test1.m
+++ Basic_Stuff/Xiu_efficient_test1.m
@@ -1,13 +1,15 @@
 clear;
 clc;
 
-f = @(x) x(:,1).*exp(x(:,2))./(1 + x(:,3).^2);
+f = @(x) cos(2*pi + 5.0*(x(:,1) + x(:,2)));%x(:,1).*exp(x(:,2))./(1 + x(:,3).^2);
 
-d = 3; %dimension of the random vector
+d = 2; %dimension of the random vector
 
-%Gauss-Hermite quadrature points (Q = q^d points)
-q = 2;
-[xi,w] = gaussQuad(q,'Hermite');
+polyBasis = 'Hermite';
+
+%Gaussian quadrature points (q^d points)
+q = 20;
+[xi,w] = gaussQuad(q,polyBasis);
 %Change the setprod in such a way that you supply a single array and 'd'-
 %DONE
 %the unique command in setprod filters out some necessary combinations if
@@ -16,19 +18,28 @@ q = 2;
 eval_pts = setprod(xi,d);
 weights = setprod(w,d);
 
+%Quadrature rule for the mean square error(Q^d points)
+Q = 20;
+[xi_mse,w_mse] = gaussQuad(Q,polyBasis);
+eval_pts_mse = setprod(xi_mse,d);
+weights_mse = setprod(w_mse,d);
 
-N = 2; %maximum degree of the multivariate polynomial
+
+order = 1;%[0,1,2,3,4,5,6]; %maximum degree of the multivariate polynomial
+MSE = [];
+for N = order
 lexOrdering = monomialDegrees(d,N);
 %Pre-computing the normalization factors
 gamma = factorial(0:N);
-
-
-P = length(lexOrdering);
+P = size(lexOrdering,1);
 fhat = zeros(P,1);
 fapprox = 0;
 for i_P = 1:P
     fhat(i_P,1) = (sum(hermite(eval_pts,lexOrdering(i_P,:)').*f(eval_pts).*prod(weights,2)))/prod(gamma(lexOrdering(i_P,:)+1));
-    fapprox = fapprox + fhat(i_P,1)*hermite(eval_pts, lexOrdering(i_P,:)');
+    fapprox = fapprox + fhat(i_P,1)*hermite(eval_pts_mse, lexOrdering(i_P,:)');
+end
+%Computing the mean squared error
+MSE = [MSE;sqrt(sum((fapprox - f(eval_pts_mse)).^2.*prod(weights_mse,2)))];
 end
 
 %plotting, works only for d=2
