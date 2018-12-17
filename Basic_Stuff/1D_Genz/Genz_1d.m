085909491c19e4915d0a2831864113d03d5e46a4
Hemaditya Malla <hemadityamalla@gmail.com>
2018-12-17 15:28:11 +0100

    Numerical Experiments

--- Basic_Stuff/1D_Genz/Genz_1d.m
+++ Basic_Stuff/1D_Genz/Genz_1d.m
@@ -6,7 +6,7 @@ g = @(xi) exp(-0.5*abs(xi)); %continuous
 %g = @(xi) 1.0./(0.5^(-2) + (xi).^2); %Product Peak 
 %g = @(xi) (1 + 0.05*xi).^(-2); %Corner peak 
 %g = @(xi) (xi > 0)*0 + (xi <= 0).*exp(0.5*xi); %Discontinuous
-g = @(xi) exp(-(0.5^2)*(xi).^2); %Gaussian peak
+%g = @(xi) exp(-(0.5^2)*(xi).^2); %Gaussian peak
 %g = @(xi) cos(2*pi*0.5 + 0.5*xi); %Oscillatory
 
 % %Some special G-function
@@ -20,7 +20,7 @@ error = [];
 error_mean = [];
 polyBasis = 'Hermite';
 %Gauss-Hermite/Legendre quadrature points (Q-points)
-Q = 100;
+Q = 500;
 [xi,w] = gaussQuad(Q,polyBasis);
 g_pts = g(xi);
 
@@ -30,28 +30,28 @@ Q_mse = 500;
 [xi_mse,w_mse] = gaussQuad(Q_mse,polyBasis);
 
 
-
-
-ip = xi;%(xi(1):0.01:xi(end))'; %Interpolating points
-
 %Number of terms in the polynomial approximation
 order = [1,4,9,14,19,24,29];
-
+%order = 1:20;
+%dlmwrite('coeff.dat',order);
+N_iter = 1;
 for N = order
+    
     gamma = factorial(0:N);
     g_hat = zeros(round(N),1); 
     g_approx = 0;
     for i=1:N+1
         polynomial = hermite(xi, i-1);
-        %g_hat(i,1) = sum(w.*polynomial.*g_pts)/(sum(w.*polynomial.*polynomial));
-        g_hat(i,1) = sum(w.*polynomial.*g_pts)/(gamma(i));
-        g_approx = g_approx + g_hat(i,1)*hermite(xi_mse,i-1);
+        g_hat(i,N_iter) = sum(w.*polynomial.*g_pts)/(sum(w.*polynomial.*polynomial));
+        %g_hat(i,1) = sum(w.*polynomial.*g_pts)/(gamma(i));
+        g_approx = g_approx + g_hat(i,N_iter)*hermite(xi_mse,i-1);
     end
+    %dlmwrite('coeff.dat',g_hat','-append');
+    
     %error = [error; abs(g_hat(1,1) - gmean)];
    %error = [error; sqrt(sum((exp(-0.5*ip.^2)/sqrt(2*pi)).*abs(g(ip) - g_approx).^2))];
    error_mean = [error_mean;abs((sum(g(xi_mse).*w_mse) - sum(g_approx.*w_mse))/sum(g(xi_mse).*w_mse))];
    error = [error;sqrt(sum(w_mse.*(g(xi_mse) - g_approx).^2))];
-   plot(xi,g_pts,'bo-','LineWidth',2);
 hold on;
 plot(xi_mse, g_approx,'LineWidth',2);
 xlim([min(xi)-0.05,max(xi)+0.05]);
--- Basic_Stuff/Tensor_Prod_Quad/Xiu_efficient_test1.m
+++ Basic_Stuff/Tensor_Prod_Quad/Xiu_efficient_test1.m
@@ -3,15 +3,15 @@ clear;clc;format long;
 f = @(x) x(:,1).*exp(x(:,2))./(1 + x(:,3).^2);
 d = 3; %dimension of the random vector
 % Input for type of orthogonal polynomial basis. Alternatively, use 'Hermite'
-polyBasis = 'Legendre';
+polyBasis = 'Hermite';
 
-q = 10;
+q = 2;
 [xi,w] = gaussQuad(q,polyBasis);
 eval_pts = setprod(xi,d); %(q^d points)
 weights = setprod(w,d);
 
 %Quadrature rule for the mean square error(Q^d points)
-Q = 25;
+Q = 15;
 [xi_mse,w_mse] = gaussQuad(Q,polyBasis);
 eval_pts_mse = setprod(xi_mse,d);
 weights_mse = setprod(w_mse,d);
@@ -31,8 +31,8 @@ for N = order
     fhat = zeros(P,1);
     fapprox = 0;
     for i_P = 1:P
-        fhat(i_P,1) = (sum(legendre(eval_pts,lexOrdering(i_P,:)').*f(eval_pts).*prod(weights,2)))/prod(gamma(lexOrdering(i_P,:)+1));
-        fapprox = fapprox + fhat(i_P,1)*legendre(eval_pts_mse, lexOrdering(i_P,:)');
+        fhat(i_P,1) = (sum(hermite(eval_pts,lexOrdering(i_P,:)').*f(eval_pts).*prod(weights,2)))/prod(gamma(lexOrdering(i_P,:)+1));
+        fapprox = fapprox + fhat(i_P,1)*hermite(eval_pts_mse, lexOrdering(i_P,:)');
     end
     %Computing the mean squared error
     MSE = [MSE;sqrt(sum((fapprox - f(eval_pts_mse)).^2.*prod(weights_mse,2)))];
