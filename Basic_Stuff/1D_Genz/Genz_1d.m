66fb4ab545e9c5fcc46ee039032b02f981a1a18c
Hemaditya Malla <hemadity@jalapeno.sc.cwi.nl>
2018-12-17 16:42:51 +0100

    Final numerical experiments

--- Basic_Stuff/1D_Genz/Genz_1d.m
+++ Basic_Stuff/1D_Genz/Genz_1d.m
@@ -3,10 +3,10 @@ clear;clc;close all; format long;
 %Defining the 1D Genz functions
 
 g = @(xi) exp(-0.5*abs(xi)); %continuous 
-%g = @(xi) 1.0./(0.5^(-2) + (xi).^2); %Product Peak 
+%g = @(xi) 1.0./(0.01^(-2) + (xi).^2); %Product Peak 
 %g = @(xi) (1 + 0.05*xi).^(-2); %Corner peak 
 %g = @(xi) (xi > 0)*0 + (xi <= 0).*exp(0.5*xi); %Discontinuous
-g = @(xi) exp(-(0.5^2)*(xi).^2); %Gaussian peak
+%g = @(xi) exp(-(0.5^2)*(xi).^2); %Gaussian peak
 %g = @(xi) cos(2*pi*0.5 + 0.5*xi); %Oscillatory
 
 % %Some special G-function
@@ -18,6 +18,7 @@ g = @(xi) exp(-(0.5^2)*(xi).^2); %Gaussian peak
 
 error = [];
 error_mean = [];
+error_denom = [];
 polyBasis = 'Hermite';
 %Gauss-Hermite/Legendre quadrature points (Q-points)
 Q = 500;
@@ -32,7 +33,7 @@ Q_mse = 500;
 
 %Number of terms in the polynomial approximation
 %order = [1,4,9,14,19,24,29];
-order = 1:29;
+order = 1:20;
 %dlmwrite('coeff.dat',order);
 N_iter = 1;
 for N = order
@@ -42,7 +43,10 @@ for N = order
     g_approx = 0;
     for i=1:N+1
         polynomial = hermite(xi, i-1);
-        g_hat(i,N_iter) = sum(w.*polynomial.*g_pts)/(sum(w.*polynomial.*polynomial));
+        gamma_num = (sum(w.*polynomial.*polynomial));
+        numer = sum(w.*polynomial.*g_pts);
+        g_hat(i,N_iter) = numer/gamma(i);
+        error_denom = [error_denom;abs((gamma(i) - gamma_num)/numer)];
         %g_hat(i,1) = sum(w.*polynomial.*g_pts)/(gamma(i));
         g_approx = g_approx + g_hat(i,N_iter)*hermite(xi_mse,i-1);
     end
@@ -68,5 +72,3 @@ hold on;
 %p = polyfit(log(order),log(error)',1);
 %loglog(order,order.^(-0.5*p(1)),'r','LineWidth',2);
 xlabel('N');ylabel('||g(\xi) - g_N(\xi)||_{L_2}')
-figure(3)
-loglog(order,error_mean,'-o','Linewidth',2,'MarkerSize',8);
