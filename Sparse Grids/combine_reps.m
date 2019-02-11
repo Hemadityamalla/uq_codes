cede08cfc281867712edce5f829dc957d11f55d7
Hemaditya Malla <hemadity@jalapeno.sc.cwi.nl>
2019-02-11 12:09:11 +0100

    Made it memory efficient, thus enabling higher orders

--- Sparse Grids/combine_reps.m
+++ Sparse Grids/combine_reps.m
@@ -7,11 +7,13 @@ function [xnew,wnew] = combine_reps(x,w)
  %2) Obtaining index list of non-unique nodes
  [C,ia,ic] = unique(snodes,'rows','stable');
  %3) Obtaining non-unique indices(NOT MEMORY EFFICIENT)
- temp = repmat(ic,[1,length(ic)]);
- temp2 = tril(temp==temp',-1);
- [~,~,non_uq_idx] = find(temp(temp2));
+%  temp = repmat(ic,[1,length(ic)]);
+%  temp2 = tril(temp==temp',-1);
+%  [~,~,non_uq_idx] = find(temp(temp2));
+%   non_uq_idx = unique(non_uq_idx,'stable');
+  %------MEMORY EFFICIENT(at least better than the above implementation)
+  non_uq_idx = find(accumarray(ic,1)-1);
  %4) Deleting the non-unique nodes and clubbing the weights
- non_uq_idx = unique(non_uq_idx,'stable');
  rep_cache = [];
  for ii=1:length(non_uq_idx)
     reps = find(ic == non_uq_idx(ii));
