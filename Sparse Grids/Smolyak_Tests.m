eb32ac587ccc4eaed46cc5e83867f91c7b39288e
Hemaditya Malla <hemadityamalla@gmail.com>
2019-02-11 08:14:31 +0100

    Test case for sparse grids

--- Sparse Grids/Smolyak_Tests.m
+++ Sparse Grids/Smolyak_Tests.m
@@ -1,21 +1,21 @@
 clear;clc;format long;
 
 %f = @(x) exp(-0.5*(abs(x(:,1)) + abs(x(:,2))));%x(:,1).*exp(x(:,2))./(1 + x(:,3).^2);
-%f = @(x) cos(2*pi*0.5 + 0.5*(x(:,1) + x(:,2)));
-f = @(x) (x(:,1) > 0)*0 .* (x(:,2) > 0)*0 + (x(:,1) <= 0).*exp(0.5*(x(:,1) + x(:,2))).*(x(:,2) <= 0);
+f = @(x) cos(2*pi*0.5 + 0.5*(x(:,1) + x(:,2)));
+%f = @(x) (x(:,1) > 0)*0 .* (x(:,2) > 0)*0 + (x(:,1) <= 0).*exp(0.5*(x(:,1) + x(:,2))).*(x(:,2) <= 0);
 d = 2; %dimension of the random vector
 % Input for type of orthogonal polynomial basis. Alternatively, use 'Hermite'
 polyBasis = 'Legendre';
 quadrature = 'Legendre';
 growth = @(x) 2.^(x-1);
-k = 6;
+k = 4;
 [eval_pts,weights] = smolyakSparseGrid(d,k,growth, quadrature);
 
 %Quadrature rule for the mean square error(Q^d points)
-Q = 10;
+Q = 5;
 [eval_pts_mse,weights_mse] = smolyakSparseGrid(d, Q, growth, quadrature);
 
-order = [0,1,2,3,4,5]; %maximum degree of the multivariate polynomial
+order = [0,1,2]; %maximum degree of the multivariate polynomial
 MSE = []; %Empty array to store the mean-squared error
 for N = order
      
