b48f7aff7ecc86e0ac9a2d5e27ce223966cab90a
Hemaditya Malla <hemadityamalla@gmail.com>
2019-02-15 08:17:41 +0100

    Numerical expts with skewness and curtosis

--- Sparse Grids/Smolyak_1d_BVP.m
+++ Sparse Grids/Smolyak_1d_BVP.m
@@ -3,12 +3,12 @@ set(0,'DefaultAxesFontSize',16,'DefaultAxesFontWeight','bold','DefaultLineLineWi
 
 Npts = 10;
 dx = 1.0/Npts;
-x = [0:dx:1]';
+x = (0:dx:1)';
 xmid = ( x(1:end-1) + x (2:end) ) / 2.0 ;
 u_left = 1;
 
 d = 5; %dimension of the random vector
-l=[1:d]';
+l=(1:d)';
 sigma = 1.0;
 % Input for type of orthogonal polynomial basis. Alternatively, use 'Hermite'
 polyBasis = 'Legendre';
@@ -23,9 +23,9 @@ exact_mean = 0.595340160418040;
 exact_variance = 0.080571966133941;
 exact_skewness = 0.285214735020665;
 exact_kurtosis = 1.493438201045323;
-%[xi_mse,w_mse] = smolyakSparseGrid(d,8,growth, quadrature);
-xi_sample = -1+2*rand(d,1e6);
-for k=[2,3,4,5,6,7,8]
+[xi_mse,w_mse] = smolyakSparseGrid(d,7,growth, quadrature);
+%xi_sample = -1+2*rand(d,1e6);
+for k=[2,3,4,5,6,7]
 
     [xi,w] = smolyakSparseGrid(d,k,growth, quadrature);
     %Rescaling to the domain [0,1]---- not sure why I am dividing by 2^d- figure it out dumbass!.
@@ -57,16 +57,17 @@ for k=[2,3,4,5,6,7,8]
     fapprox = 0;
     for i_P = 1:P
         fhat(i_P,1) = (sum(legendre(xi',lexOrdering(i_P,:)').*u_mid'.*prod(w',2)))/prod(gamma(lexOrdering(i_P,:)+1));
-        fapprox = fapprox + fhat(i_P,1)*legendre(xi_sample', lexOrdering(i_P,:)');
+        fapprox = fapprox + fhat(i_P,1)*legendre(xi_mse', lexOrdering(i_P,:)');
     end
-    %ap_var = dotprod((fapprox' - fhat(1,1)).^2,w_mse');
+    ap_var = dotprod((fapprox' - fhat(1,1)).^2,w_mse');
     %ap_skew = dotprod(((fapprox' - fhat(1,1))/sqrt(ap_var)).^3,w_mse');
-    %ap_kurt = dotprod(((fapprox' - fhat(1,1))/sqrt(ap_var)).^4,w_mse');
+    ap_kurt = dotprod(((fapprox' - fhat(1,1))/sqrt(ap_var)).^4,w_mse');
     fhat(1,1)
     error_mean = [error_mean;abs(fhat(1,1) - exact_mean)];
-    error_var = [error_var;abs(variance(fapprox) - exact_variance)];
+    error_var = [error_var;abs(var(fapprox) - exact_variance)];
     error_skew = [error_skew;abs(skewness(fapprox,1) - exact_skewness)];
     error_kurt = [error_kurt;abs(kurtosis(fapprox,1) - exact_kurtosis)];
     numpts = [numpts;length(w)];
-
+    %CHECK FOR THE CORRECTNESS OF YOUR FORMULAE USING AN EXAMPLE WITH EXACT
+    %VALUES!!!!!!!
 end
