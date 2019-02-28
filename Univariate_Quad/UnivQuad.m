2641445d6d9b6dccab7944e19b82e07e5768161c
Hemaditya Malla <hemadity@jalapeno.sc.cwi.nl>
2019-02-28 14:20:28 +0100

    Numerical experiments

--- Univariate_Quad/UnivQuad.m
+++ Univariate_Quad/UnivQuad.m
@@ -1,23 +1,20 @@
 clear;format long;
 set(0,'DefaultAxesFontSize',16,'DefaultAxesFontWeight','bold','DefaultLineLineWidth',2,'DefaultLineMarkerSize',8);
 
-N = 5;
+N = 50;
 %Generate the set of points
 k=1:N;
 x = cos((pi*(k-1))/(N-1));
 
-%Defining the weighting function 
-%w = @(x) 0.5; %Uniform distribution
-
 %Vandermonde matrix
-V = fliplr(vander(x));
+V = fliplr(vander(x))';
 %V = vander_cheb(N,x');
 
 
 %RHS integral vector ( (x^n)/(n+1) )
 
 b = zeros(N,1);
-b(:,1) = 0.5*(1./k).*((1).^(k) - (-1).^(k));
+b(:,1) = (1./k).*((1).^(k) - (-1).^(k));
 
 
 %Weights
--- Univariate_Quad/testbasis_exp.m
+++ Univariate_Quad/testbasis_exp.m
@@ -7,7 +7,7 @@ f = @(x,k) exp(a*x).*(x).^k;
 % g = @(x,k) exp(-a*x).*(x).^k;
 
 %Generate the set of points
-x = rand(N,1); %cos((pi*(k-1))/(N-1))
+x = cos((pi*(0:N-1))/(N-1)); %cos((pi*(k-1))/(N-1))
 
 %Defining the weighting function 
 %w = @(x) 0.5; %Uniform distribution
@@ -20,8 +20,9 @@ V = vander_analogue(N, x, f);
 %RHS integral vector
 Eint = @(x,k) (exp(a*x)/a^(k+1)).*(((-1).^(0:k)).*(factorial(0:k))*((a*x).^(k-(0:k)))');
 b = zeros(N,1);
-for idx = 1:N
-    b(idx,1) = 0.5*(Eint(1,idx-1)-Eint(-1,idx-1));
+b(1,1) = N; 
+for idx = 2:N
+    b(idx,1) = (Eint(1,idx-1)-Eint(-1,idx-1));
 end
 
 %Weights
