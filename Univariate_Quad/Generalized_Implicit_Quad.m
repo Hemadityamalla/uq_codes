ff24dfab9f82c8680d70106bf7a42f58d7d069e3
Hemaditya Malla <hemadity@jalapeno.sc.cwi.nl>
2019-03-21 14:44:03 +0100

    Numerical experiments

--- Univariate_Quad/Generalized_Implicit_Quad.m
+++ Univariate_Quad/Generalized_Implicit_Quad.m
@@ -11,15 +11,15 @@ for N = [5:5:30]
     mean_error = 0.0;
 for iter = 1:10
     %QRule1
-    %f = @(x,k) (mod(k-1,2)==0).*(x.^((k-1)/2)) + (mod(k-1,2)==1).*(log(x).*x.^(k/2)); coeffs = 1:N;
+    f = @(x,k) (mod(k-1,2)==0).*(x.^((k-1)/2)) + (mod(k-1,2)==1).*(log(x).*x.^(k/2)); coeffs = 1:N;
     %QRule2
     %f = @(x,k) (mod(k-1,2)==0).*(x.^((k-1)/2)) + (mod(k-1,2)==1).*(x.^(k/2 + 1/3)); coeffs = 1:N;
     %QRule3
     %f = @(x,a) exp(x.*(a-1)); coeffs = [1,rand(1,N-1)]; coeffs = [1,rand(1,N-1)];
     %QRule4
-    f = @(x,k) (mod(k-1,2)==0).*(x.^((k-1)/2)) + (mod(k-1,2)==1).*(sin(x).*x.^(k/2)); coeffs = 1:N;
+    %f = @(x,k) (mod(k-1,2)==0).*(x.^((k-1)/2)) + (mod(k-1,2)==1).*(sin(x).*x.^(k/2)); coeffs = 1:N;
     %QRule5
-    
+    %f = @(x,k) 1./(1 + k*x); coeffs = 1 - 1./sqrt(1:N); 
 
     %Initialize quad rule
     x = y(1:N);
@@ -32,9 +32,6 @@ for iter = 1:10
        w = [((D)/(D+1))*w;1./(D+1)];
        %Update weights
        V = general_vandermonde(x, f, coeffs);
-       %orthonormalize the basis
-       [Q,R] = qr(V',0);
-       V = Q';
        nullVec = null(V);
        c = nullVec(:,1);
 
@@ -53,33 +50,34 @@ for iter = 1:10
     %Numerical tests
 
     %1) Integration with monomial of order N-1
-    %exact = 1/(N);
-    %approx=dotprod(x'.^(N-1),w);
+%     exact = mean(y'.^(N-1));%1/(N);
+%     approx=dotprod(x'.^(N-1),w);
 
     %2) Integration with cont. Genz function
-    %exact = 1 - exp(-0.25);
-    %approx = dotprod(0.25*exp(-0.5*abs(x' - 0.5)),w);
+%     exact = mean(0.25*exp(-0.5*abs(y' - 0.5)));%1 - exp(-0.25);
+%     approx = dotprod(0.25*exp(-0.5*abs(x' - 0.5)),w);
 
 
     %3) Integration with corner peak Genz function
-    exact = mean(1./(1 + 5*y').^2);%1./6;
-    approx = dotprod(1./(1 + 5*x').^2, w);
+%     exact = mean(1./(1 + 5*y').^2);%1./6;
+%     approx = dotprod(1./(1 + 5*x').^2, w);
     
     %4) Integration with corner peak Genz function
-     %exact = mean(y'.^45.*log(y'));%-1./2116;
-     %approx = dotprod((x'.^45).*log(x'), w);
+%      exact = mean(y'.^45.*log(y'));%-1./2116;
+%      approx = dotprod((x'.^45).*log(x'), w);
     
     %5) Integration with highly oscillatory function
-%     exact = (2./55)*sin(55./2)^2;
-%     approx = dotprod(sin(55*x'),w);
+    exact = mean(sin(55*y'));%(2./55)*sin(55./2)^2;
+    approx = dotprod(sin(55*x'),w);
     mean_error = mean_error + abs(exact - approx);
 end
      mean_error = mean_error/iter
-%     if N == 5
-%         dlmwrite('Errors_quad6_fn6.dat',mean_error);
-%     else
-%         dlmwrite('Errors_quad6_fn6.dat',mean_error,'-append');
-%     end
+     fname = 'Errors_quad1_fn5.dat';
+    if N == 5
+        dlmwrite(fname,mean_error);
+    else
+        dlmwrite(fname,mean_error,'-append');
+    end
 end
  
 %3) Integration with osc. Genz function
