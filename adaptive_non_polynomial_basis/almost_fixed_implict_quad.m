0fc79cbcbd8e63a1327afdd77321f233bb7c017c
Hemaditya Malla <hemadity@jalapeno.sc.cwi.nl>
2019-04-24 14:23:46 +0200

    Numerical test where one row is replaced by the function- FAILED EXPERIMENT

---
+++ adaptive_non_polynomial_basis/almost_fixed_implict_quad.m
@@ -0,0 +1,33 @@
+%This is trivial and wrong as we are just adding the function evaluations
+%and trying to obtain a quadrature rule for it.
+
+function [x,w,y] = almost_fixed_implict_quad(N,f, Kmax)
+    y = rand(Kmax,1); %Uniform distribution
+    %Initialize quad rule
+    x = y(1:N);
+    w = ones(N,1)/(N);
+    %Implicit quad rule
+    for D=N:(Kmax-1)
+       %Node addition
+       x = [x;y(D+1)];
+       w = [((D)/(D+1))*w;1./(D+1)];
+       %Update weights
+       V = general_vandermonde(x, @(x,k) x.^(k-1), 1:length(x));
+       %Replacing the second row with an interpolation of the function
+       V(2,:) = f(x); %This is a bad idea as x keeps changing every iteration and each function evaluation is really expensive
+
+       nullVec = null(V); %Wait, why do I neglect the last column?!
+       c = nullVec(:,1);
+
+       % Apply Caratheodory's and determine the node that will be removed
+        [alpha, k] = min(w(c>0)./c(c>0));
+        %[alpha, k] = max(w(c<0)./c(c<0));
+        id = find(c > 0);%find(c < 0)
+        k = id(k);
+
+        %Node removal
+        w = w-alpha*c;
+        x(k, :) = []; w(k) = [];
+    end
+
+end
