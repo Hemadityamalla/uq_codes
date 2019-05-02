3dcd02aab644c8a98d806a45d34d753b789b8f3e
Hemaditya Malla <hemadity@jalapeno.sc.cwi.nl>
2019-05-02 15:21:47 +0200

    a small numerical experiment to verify that the code is wrong

--- adaptive_non_polynomial_basis/adaptive_quad_test.m
+++ adaptive_non_polynomial_basis/adaptive_quad_test.m
@@ -1,13 +1,13 @@
 close all;clear;clc;format long;
 
 %points used for piecewise linear interpolation
-Kmax = 1e4;
+Kmax = 1e2;
 
 %f = @(x) 1.0*(x > 0.5);%Test function to be integrated
 f = @(x) x.*(x < 0.5) + (x >= 0.5).*exp(-0.5*x);
 %f = @(x) exp(-abs(x - 0.5)/2)/sqrt(2*pi);
 %f = @(x) exp(-x.^2/2/sqrt(2*pi));
-N = 5:5:100;
+N = 5:5:50;
 error = [];
 for i = N
     %[xt,wt,samples] = almost_adaptive_quad(i,f,Kmax);
